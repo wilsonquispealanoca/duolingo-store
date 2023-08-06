31248d018c7a0f508e7d620916b97efde2230568
wilsonquispealanoca <wildiscjockey@gmail.com>
2023-08-06 16:58:10 -0400

    UPDATE: styles navbar

--- src/components/details/details.tsx
+++ src/components/details/details.tsx
@@ -13,11 +13,6 @@ export default component$<DetailsProps>(({ title, subtitle }) => {
     <div>
       <h2 class="detail-title">{title}</h2>
       <p class="detail-description">{subtitle}</p>
-      <List
-        text1="10” tall"
-        text2="This item ships from the USA"
-        text3="Suitable for ages 14+"
-      />
     </div>
   );
 });
